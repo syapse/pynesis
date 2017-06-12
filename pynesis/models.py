2c4042aa2c9f5a9b25aa781cac45a668162dbd17
Matias Surdi <matias.surdi@ticketea.com>
2017-06-12 17:48:55 +0200

    Add app label to Django checkpoint persistence model

--- pynesis/models.py
+++ pynesis/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Checkpoint(models.Model):
     key = models.CharField(max_length=255, primary_key=True)
     checkpoints = models.TextField()
+    class Meta:
+        app_label = "pynesis"
