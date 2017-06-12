df49aa9561fba4565a1c6699bc3bbe394871d1dc
Jorge Maroto <patoroco@gmail.com>
2017-06-12 18:04:58 +0200

    Making linter happy

--- pynesis/models.py
+++ pynesis/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Checkpoint(models.Model):
     key = models.CharField(max_length=255, primary_key=True)
     checkpoints = models.TextField()
+
     class Meta:
         app_label = "pynesis"
